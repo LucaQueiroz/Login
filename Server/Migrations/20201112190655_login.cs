91a5156ae35793f4bbf161a305a43d8f90446baa
LucaQueiroz <luca-queiroz@univem.edu.br>
2020-11-18 15:06:36 -0300

    login

--- Server/Migrations/20201112190655_login.cs
+++ Server/Migrations/20201112190655_login.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using blazor_mysql2.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace blazormysql2.Server.Migrations
 {
+     class MyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Title = "Sr",
+                FirstName = "Luis",
+                MiddleName = "Guilherme",
+                LastName = "Sobral",
+                DateOfBirth = Convert.ToDateTime("19990331"),
+                Email = "sobrazinhoOchave@gmail.com",
+                Password = "123456",
+                ConfirmPassword = "123456",
+                AcceptTerms = true
+            }
+    );
+        }
+        #endregion
+    }
     public partial class login : Migration
     {
+       
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
